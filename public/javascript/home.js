1f53bf12b57cca66d88c1147b10481eabfa3b0b7
Diana <dianaltecson@gmail.com>
2017-12-30 11:34:15 -0800

    able to send back data for comment updates

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,23 +1,29 @@
 $(function() {
     $('.modal').modal();
 
+    // clicking the add comment button triggers the modal for comment inputs
+        // attaches data-articleId attribute to pass on the articleId to be stored with the comment upon submission
     $('.add-comment-btn').click(function () {
-        console.log('clicked');
         var articleId = $(this).attr('id');
-        console.log(articleId);
-        // $.ajax({
-        //     method: 'POST',
-        //     url: '/newcomment',
-        //     data: articleId,
-        //     success: function () {
-
-        //     }
-        // });
+        $('#submit-comment-btn').attr('data-articleId', articleId);
     });
 
     $('#submit-comment-btn').click(function () {
-        console.log($('#comment-title').val());
-        console.log($('#comment-body').val());
+        var articleId = $(this).attr('data-articleId');
+        var commentTitle = $('#comment-title').val();
+        var commentBody = $('#comment-body').val();
+        $.ajax({
+            method: 'PUT',
+            url: '/updatecomment',
+            data: {
+                articleId: articleId,
+                title: commentTitle,
+                body: commentBody
+            },
+            success: function () {
+                console.log('worked');
+            }
+        })
 
-    })
+    });
 });
