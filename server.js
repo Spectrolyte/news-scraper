d2ec284c5ec2e86804b3f7f033e2bbc2ecd4cc65
Diana <dianaltecson@gmail.com>
2017-12-30 13:56:35 -0800

    added dynamic port

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var cheerio = require("cheerio");
 var db = require("./models");
 var router = require('./controllers/htmlRoutes.js');
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/news-scraper";
 
 // Initialize Express
